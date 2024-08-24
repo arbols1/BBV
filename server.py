caa4546d9659ba7a8be522a777d6c28246f79eb7
arbols1 <ag277952@gmail.com>
2024-08-24 14:39:20 +0000

    Mundo guardado 2024-08-24

--- server.py
+++ server.py
@@ -10,7 +10,7 @@
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
-# eu		      Eur1opa (Frankfurt)
+# eu		      Europa (Frankfurt)
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
 # sa		      Sudamérica (São Paulo)
